8e4b54df920bb5a2d5814b9348ee0b0c58c19bde
Mona <mona.roettger@gmail.com>
2016-04-19 15:49:06 +0200

    Methode onOpen ersetzt

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -32,7 +32,17 @@ public class ImageEditorFrame extends JFrame{
         public void actionPerformed(ActionEvent e){ onOpen(); }
      });
   }
-  private void onOpen(){ 
-   JOptionPane.showMessageDialog(this, "Open Selected");
-  }
+ private void onOpen(){
+	 try{
+		 JFileChooser fileChooser = new JFileChooser();
+		 fileChooser.showOpenDialog(this);
+		 File file = fileChooser.getSelectedFile();
+		 BufferedImage image = ImageIO.read(file);
+		 panel.setImage(image);
+		 }
+		 catch(IOException e){
+		 JOptionPane.showMessageDialog(this,
+		 "Die Datei konnte nicht geöffnet werden");
+		 }
+	}
 }
