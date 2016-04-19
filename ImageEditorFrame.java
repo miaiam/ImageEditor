daf7f4a2a304c3d87d442b337be0b02b4c272148
Mia Wölm <Mia@ws-65-6.haw-1x.haw-hamburg.de>
2016-04-19 15:36:25 +0200

    added ImagedEditorPanel

--- ImageEditorFrame.java
+++ ImageEditorFrame.java
@@ -5,10 +5,22 @@ import java.awt.image.*;
 import java.io.*;
 import javax.imageio.*;
 public class ImageEditorFrame extends JFrame{
+	private ImageEditorPanel panel;
  public ImageEditorFrame(){
  setTitle("Image Editor");
+ panel = new ImageEditorPanel();
+ add(panel);
+ setDummyImage();
  setDefaultCloseOperation(EXIT_ON_CLOSE);
  setSize(400, 300);
  setVisible(true);
  }
+ private void setDummyImage(){
+ BufferedImage bufferedImage =
+ new BufferedImage(400, 300, BufferedImage.TYPE_INT_RGB);
+ Graphics g = bufferedImage.getGraphics();
+ g.setColor(Color.YELLOW);
+ g.fillOval(10, 10, 380, 280);
+ panel.setImage(bufferedImage);
+}
 }
